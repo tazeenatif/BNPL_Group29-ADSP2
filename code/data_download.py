6ee1ceadc24395a023ae9f804ef0009ba6426211
Tazeen Atif <tazeen.atif20@gmail.com>
2026-08-31 11:25:45 +1000

    count victorian sa2 districts in 2021

--- code/data_download.py
+++ code/data_download.py
@@ -8,5 +8,8 @@ sa2_2021 = pd.read_excel("data/external/SA2_2021_AUST.xlsx")
 #filter for just Victorian districts
 vic_sa2_21 = sa2_2021[sa2_2021["STATE_NAME_2021"] == "Victoria"].copy()
 sa2districts_21 = vic_sa2_21["SA2_NAME_2021"].unique()
-print("2021 Victorian SA2s:")
+
+#list of victorian sa2 districts
+print("2021 Victorian SA2 districts:")
+print(f"There are {len(sa2districts_21)} registered Victorian SA2 districts from the 2021 ASGS ABS dataset.")
 print(sa2districts_21)
